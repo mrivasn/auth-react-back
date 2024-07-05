f00bf25acfed49bc920b82614385ea86de9bda05
mrivasn <mrivasn.mrn@gmail.com>
2024-07-05 17:17:34 -0400

    Se agrega validación de formulario para Login

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const {jsonResponse} = require("../lib/jsonResponse");
 
-router.get("/", (req, res) => {
-    res.send("login")
+router.post("/", (req, res) => {
+
+    const { username, password } = req.body;
+    if (!!!username || !!!password){
+        return res.status(400)
+                .json(jsonResponse(400, { error: "Fields are required!" }))
+    }
+
+    //autenticar usuario en la BD
+    const accessToken = "access_token";
+    const refreshToken = "refresh_token";
+    const user = {
+        id: "1",
+        name: "user test",
+        username: "userTest"
+    };
+
+    res.status(200)
+        .json(jsonResponse(200, { user, accessToken, refreshToken }))
+
 });
 
 module.exports = router;
